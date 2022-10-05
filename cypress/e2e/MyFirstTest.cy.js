b51dcdfd8fdedb8684dadb0d001c3e17787d8ca0
Mikail Sonmez <mikail4458@hotmail.com>
2022-10-06 00:03:01 +0300

    v2

---
+++ cypress/e2e/MyFirstTest.cy.js
@@ -0,0 +1,34 @@
+
+
+describe('My First Test', () => {
+
+  it('verify title-positive test', () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+    cy.title().should('eq','OrangeHRM')
+  })
+
+  it('verify title-Negative test', () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+    cy.title().should('eq','OrangeHRM123')
+  })
+
+  it('test3', () => {
+
+    // steps
+
+  })
+
+  it('test4', () => {
+
+    // steps
+
+  })
+
+})
+
+
+describe('My Second Test', function() {
+  it('Does not do much!', function(){
+    expect(true).to.equal(true)
+  })
+})
